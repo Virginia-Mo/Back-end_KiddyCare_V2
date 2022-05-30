bfdd34bad372e3ea60a1890166b2088206bb2019
Virginia-Mo <antonio.vir@hotmail.com>
2022-05-31 00:13:59 +0200

    Javascript added

--- js/script.js
+++ js/script.js
@@ -7,9 +7,48 @@ toggle.addEventListener("click", function() {
   body.classList.toggle("openMenu");
 });
 
-// let blogMenuContainer = document.querySelector(".pageMenuContainer");
-// let doubleLink = document.querySelectorAll(".doubleLInk");
+let blogMenuContainer = document.querySelector(".pageMenuContainer");
+let doubleLink = document.querySelectorAll(".doubleLInk");
 
-// blogMenuContainer.addEventListener("click", function(){
-//   menu.classList.toggle("active");
-// });
+blogMenuContainer.addEventListener("click", function(){
+  body.classList.toggle("active");
+});
+
+
+let rev = 0;
+    
+        const prev = document.getElementById("previous")
+        const next = document.getElementById("after")
+        let reviews = document.getElementsByClassName("parents");
+        let bodyT = document.querySelector("body")
+        
+        prev.addEventListener("click", previousReview)
+        function previousReview() {
+            rev = rev - 1;
+            bodyT.classList.add("activeTest")
+            carousel(rev);
+        }
+        next.addEventListener("click", nextReview)
+        function nextReview() {
+            rev = rev + 1;
+            bodyT.classList.add("activeTest")
+            carousel(rev);
+        }
+  
+        function carousel(review) {
+            let reviews = document
+                .getElementsByClassName("parents");
+  
+            if (review >= reviews.length) {
+                review = 0;
+                rev = 0;
+            }
+            if (review < 0) {
+                review = reviews.length - 1;
+                rev = reviews.length - 1;
+            }
+            for (let i = 0; i < reviews.length; i++) {
+                reviews[i].style.display = "none";
+            }
+            reviews[review].style.display = "block";
+        }    carousel(rev);
