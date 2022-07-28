c1818a9af2e25a5d062a238fbda5f3dfe8ddd5ab
Virginia Mo <antonio.vir@hotmail.com>
2022-07-28 12:14:14 +0200

    test

--- app/database.js
+++ app/database.js
@@ -3,6 +3,11 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   define : {
     underscored: true
-  }
+  },
+  dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: true
+    }
 });
 module.exports = sequelize;
