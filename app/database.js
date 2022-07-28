7cc1f3aac6132a29feaff4d06bf2024dc98d41f7
Virginia Mo <antonio.vir@hotmail.com>
2022-07-28 12:56:21 +0200

    test

--- app/database.js
+++ app/database.js
@@ -12,6 +12,9 @@
 // });
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    define : {
+    underscored: true
+    },
   dialectOptions: {
     ssl: {
       require: true,
