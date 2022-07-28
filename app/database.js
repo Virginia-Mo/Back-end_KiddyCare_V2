c1ff5d71ac5ca89243e8b0b943ff5aeceee286e5
Virginia Mo <antonio.vir@hotmail.com>
2022-07-28 12:49:39 +0200

    test

--- app/database.js
+++ app/database.js
@@ -1,13 +1,32 @@
-const { Sequelize } = require('sequelize');
+// const { Sequelize } = require('sequelize');
 
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//   define : {
+//     underscored: true
+//   },
+//   dialect: 'postgres',
+//     protocol: 'postgres',
+//     dialectOptions: {
+//         ssl: true
+//     }
+// });
+const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  define : {
-    underscored: true
-  },
-  dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-        ssl: true
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
+  }
+}
+);
+
+sequelize
+.authenticate()
+.then(() => {
+  console.log('Connection has been established successfully.');
+})
+.catch(err => {
+  console.error('Unable to connect to the database:', err);
 });
 module.exports = sequelize;
